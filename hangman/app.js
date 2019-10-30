f0edd736a805de20cf1fa17b9a6ac1203a1bb281
ChristianPerryDotDev <54387644+ChristianPerryDotDev@users.noreply.github.com>
2019-10-30 03:37:25 -0700

    (103-106) Word Puzzle and Country Code API integrations

--- hangman/app.js
+++ hangman/app.js
@@ -5,7 +5,7 @@ const game1 = new Hangman('Car Parts', 2)
 puzzleEl.textContent = game1.puzzle //getPuzzle()
 guessesEl.textContent = game1.statusMessage //getStatus()
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.status === 'playing' ? game1.handleGuess(guess) : doNothing()
     puzzleEl.textContent = game1.puzzle
@@ -13,4 +13,35 @@ window.addEventListener('keypress', function (e) {
 })
 
 const doNothing = () => {}
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+request.send()
+
+const countryCode = "CA"
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    } else if (e.target.readyState === 4) {
+        console.log('y u no work')
+    }
+})
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
 
